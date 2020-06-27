cb93f48bc6054b1d31e8ed8fa4207fb8f82bb223
Ken Hwang <ken.r.hwang@gmail.com>
2020-06-27 17:16:28 -0400

    Setup DNS delegation to Azure DNS with A and CNAME records

--- aks/aks-setup.sh
+++ aks/aks-setup.sh
@@ -2,6 +2,7 @@ GROUPNAME=k-eco
 LOCATION=eastus
 CLUSTERNAME=k-cluster
 NODECOUNT=2
+DOMAIN_NAME=kenrhwang.com
 
 # Create resource group
 az group create -g $GROUPNAME -l $LOCATION
@@ -21,4 +22,16 @@ CLUSTER_RESOURCE_GROUP=$(az aks show  -g k-eco -n k-cluster --query nodeResource
 PUBLIC_IP=$(az network public-ip create --resource-group $CLUSTER_RESOURCE_GROUP --name k-cluster-public-ip --sku Standard --allocation-method static --query publicIp.ipAddress -o tsv)
 
 # Create DNS zone
-#PUBLIC_IP_ID=$(az network public-ip list --query "[?ipAddress=='$PUBLIC_IP'].id" -o tsv)
+PUBLIC_IP_ID=$(az network public-ip list --query "[?ipAddress=='$PUBLIC_IP'].id" -o tsv)
+az network dns zone create -g $GROUPNAME -n $DOMAIN_NAME
+
+# Create temporary A record
+az network dns record-set a add-record -g $GROUPNAME --record-set-name @ --zone-name $DOMAIN_NAME --ipv4-address 1.1.1.1
+
+# Update temporary record to point to public IP resource
+az network dns record-set a update --name @ -g $GROUPNAME --zone-name $DOMAIN_NAME --target-resource $PUBLIC_IP_ID
+
+# Create CNAME records
+az network dns record-set cname set-record -g $GROUPNAME --zone-name $DOMAIN_NAME --record-set-name api --cname $DOMAIN_NAME
+az network dns record-set cname set-record -g $GROUPNAME --zone-name $DOMAIN_NAME --record-set-name '*' --cname $DOMAIN_NAME
+
